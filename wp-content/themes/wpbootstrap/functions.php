0ae5db2223869988b956af788d707f4700601106
imsolayman <contact@solayman.me>
2019-03-09 00:38:01 +0600

    blog page with posts done

--- wp-content/themes/wpbootstrap/functions.php
+++ wp-content/themes/wpbootstrap/functions.php
@@ -4,6 +4,7 @@
 
     // Theme support
     function wpb_theme_setup(){
+        add_theme_support('post-thumbnails');
         // Nav menus
         register_nav_menus(array(
             'primary' => __('Primary Menu')
@@ -11,3 +12,10 @@
     }
 
     add_action('after_setup_theme', 'wpb_theme_setup');
+
+    // Excerpt Length control
+    function set_excerpt_length(){
+        return 45;
+    }
+
+    add_filter('excerpt_length', 'set_excerpt_length');
